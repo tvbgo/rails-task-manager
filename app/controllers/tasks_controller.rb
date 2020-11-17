fb0fd405388e94f6465f0c03cfdd2df05ba3644c
Tomas Gomes <tvb.gomes@gmail.com>
2020-11-17 14:17:50 -0300

    read all and read one implemented

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'index', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks/new', to: 'tasks#create'
 end
